dcde017883d9f13d503e1ce43d22eafb3bcd5cdb
KilonzoJames <jay.kilonzo@gmail.com>
2023-06-19 13:27:27 +0300

    javascript-functions-continued

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(activity='roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+}console.log(saturdayFun('bathe my dog'))
+
+function mondayWork(work='go to the office'){
+    return `This Monday, I will ${work}.`
+}console.log(mondayWork('sleep'))
+
+function wrapAdjective(visual='*',slash='||'){
+    return function(adjective="a hard worker"){
+    return `You are ${visual}${adjective}${visual}!`};
+}const encouragingPromptFunction = wrapAdjective("*")();
+console.log(encouragingPromptFunction);
